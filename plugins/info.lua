5e488435abe266298d40be73e6037c6006ae1647
mahdib9 <mahdib9@users.noreply.github.com>
2016-02-20 11:59:41 +0000

    Update info.lua

--- plugins/info.lua
+++ plugins/info.lua
@@ -208,7 +208,7 @@ local function run(msg, matches)
 	 text = text..'› نام گروه : '..msg.to.title..'\n'
      text = text..'› کد گروه : '..msg.to.id
     end
-	text = text..'\nِ'
+	text = text..'\nِ' @mahdib9'
     return send_msg(receiver, text, ok_cb, true)
     end
   end
