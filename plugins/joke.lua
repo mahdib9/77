847eb30563b370fc88eec5d784358c2b0f2e5e47
mahdib9 <mahdib9@users.noreply.github.com>
2016-02-26 08:09:04 +0000

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,21 +1,13 @@
-    --Joke Plugin v1.0 By @AmirDark
-    --https://github.com/amirhmz
-    --Telegram.me/AmirDark
-local database = 'http://vip.opload.ir/vipdl/94/11/amirhmz/'
+local database = 'http://mpfan.ir/'
 local function run(msg)
-	local res = http.request(database.."joke.db")
-	local joke = res:split(",")
-	return joke[math.random(#joke)]
+local res = http.request(database.."joke.db")
+local joke = res:split(",")
+return joke[math.random(#joke)]
 end
---Joke Plugin v1.0 By @AmirDark
+
 return {
-	description = "500 Persian Joke",
-	usage = "!joke : send random joke",
-	patterns = {
-		"^[/!]joke$",
-		"^(joke)$"
-		},
-	run = run
+description = "500 Persian Joke",
+usage = "!joke : send random joke",
+patterns = {"^[/!]جک$"},
+run = run
 }
-    --https://github.com/amirhmz
-    --Telegram.me/AmirDark
