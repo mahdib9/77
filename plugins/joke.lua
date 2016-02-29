137231e6a5d1e751a1870bc11b69426be0e2a65b
Mahdi <mahdib9@users.noreply.github.com>
2016-02-29 12:06:18 +0000

    Update joke.lua

--- plugins/joke.lua
+++ plugins/joke.lua
@@ -1,13 +1,10 @@
-local database = 'http://mpfan.ir/'
+local address = 'http://powerful.friends-memorial.ir/api/'
 local function run(msg)
-local res = http.request(database.."joke.db")
-local joke = res:split(",")
-return joke[math.random(#joke)]
+local resolve = http.request(address..'index.php')
+resolve = string.gsub(resolve,'@GPMod','')
+return resolve
 end
-
 return {
-description = "500 Persian Joke",
-usage = "!joke : send random joke",
-patterns = {"^[/!]جک$"},
-run = run
+   patterns = {"^[/!]time$"},
+   run = run
 }
